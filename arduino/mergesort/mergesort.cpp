91b31094537377538e96929f6fcad655ceb3227c
Adam Ford <adam.ford@greatbigsolutions.com>
2012-11-07 11:45:22 -0700

    Attempt to implement mergesort

--- arduino/mergesort/mergesort.cpp
+++ arduino/mergesort/mergesort.cpp
@@ -8,6 +8,7 @@ int R[n_r];
 
 int S[n_l + n_r];
 
+
 void merge(int *left, int n_Left, int *right, int n_Right, int *s) {
   // position of the next element to be processed
   int left_pos = 0;
@@ -16,15 +17,20 @@ void merge(int *left, int n_Left, int *right, int n_Right, int *s) {
   // position of next element of S to be specified
   // note: s_pos = left_pos + right_pos
   int s_pos = 0;
+  int pickFromLeft = 0;
 
   for(int s_pos = 0; s_pos < (n_Left + n_Right); s_pos++) {
-    if (left_pos == n_Left) {
-      s[s_pos] = right[right_pos];
-      right_pos++;
-    } else if (right_pos == n_Right) {
-      s[s_pos] = left[left_pos];
-      left_pos++;
+    if (left_pos >= n_Left) {
+      pickFromLeft = 0;
+    } else if (right_pos >= n_Right) {
+      pickFromLeft = 1;
     } else if (left[left_pos] <= right[right_pos]) {
+      pickFromLeft = 1;
+    } else {
+      pickFromLeft = 0;
+    }
+
+    if(pickFromLeft) {
       s[s_pos] = left[left_pos];
       left_pos++;
     } else {
@@ -34,9 +40,64 @@ void merge(int *left, int n_Left, int *right, int n_Right, int *s) {
   }
 }
 
+void mergesort(int *A, int length) {
+  if(length <= 1) {
+    return;
+  }
+  if(length == 2) {
+    if ( A[0] > A[1] ) {
+      int tmp = A[0];
+      A[0] = A[1];
+      A[1] = tmp;
+    }
+    return;
+  }
+
+  // split A in half
+  // A[0 .. midpoint - 1], A[midpoint .. length]
+  int midpoint = length / 2;
+
+  mergesort(A, midpoint);
+  mergesort(A+midpoint, length-midpoint);
+
+  int S[length];
+  merge(A, midpoint, A+midpoint, length - midpoint, S);
+
+  for(int i=0; i < length; i++) {
+    A[i] = S[i];
+  }
+}
+
 void setup() {
   Serial.begin(9600);
   randomSeed(analogRead(12));
+
+int test_len = 32;
+int test[test_len];
+
+Serial.print("In: ");
+for(int i =0; i < test_len; i++) {
+  test[i] = random(0, 100);
+  Serial.print(test[i]);
+  Serial.print(" ");
+}
+
+Serial.println(" ");
+
+mergesort(test, test_len);
+
+Serial.print("Out: ");
+for(int i =0; i < test_len; i++) {
+  test[i] = random(0, 100);
+  Serial.print(test[i]);
+  Serial.print(" ");
+}
+
+Serial.println(" ");
+
+
+
+  /*
 
   Serial.print("L: ");
   for (int i = 0; i < n_l; i++) {
@@ -63,6 +124,9 @@ void setup() {
     Serial.print(S[i]);
     Serial.print(" ");
   }
+
+  Serial.println(" ");
+  */
 }
 
 void loop() {
