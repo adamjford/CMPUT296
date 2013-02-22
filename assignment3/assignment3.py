97e2f7a9c24977f9093271c139fd3cf2fa5d6547
adamjford <adamjford@gmail.com>
2013-02-21 21:21:51 -0700

    Assignment 3: Add tests

--- assignment3/assignment3.py
+++ assignment3/assignment3.py
@@ -3,15 +3,34 @@ from least_cost_path import least_cost_path
 from testcompare import tcompare
 import math
 
-# contents: { vertex_id: ( lat, long ) }
-vertices = {}
-# contents: { ( v1, v2 ): name }
-edges = {}
+class Server:
+    def __init__(self):
+        # contents: { vertex_id: ( lat, long ) }
+        self.vertex_locations = {}
+        # contents: { ( v1, v2 ): name }
+        self.edges = {}
+        self.graph = Digraph()
+
+    def add_vertex(vertex_id, lat, long):
+        """
+        Adds a vertex and its location to the graph.
+
+        >>> s = Server()
+        """
+
+        pass
 
 def cost_distance(e):
     """
     cost_distance returns the straight-line distance between the two
     vertices at the endpoints of the edge e.
+
+    >>> vertices = {}
+    >>> vertices[275965046] = (53.473513,-113.5199716)
+    >>> vertices[283173961] = (53.4590515,-113.4263165)
+    >>> tcompare(0.094765, cost_distance((275965046, 283173961)))
+    True
+
     """
     v1 = vertices[e[0]]
     v2 = vertices[e[1]]
@@ -21,7 +40,7 @@ def cost_distance(e):
 
 def euclidean_distance(coords1, coords2):
     """
-    Returns the Euclidean distance between two coordinates
+    Returns the Euclidean (straight-line) distance between two coordinates
 
     >>> tcompare(0, euclidean_distance((0,0), (0,0)))
     True
