dd19456aefd99e1c532733671757d45a4fdef4f1
adamjford <adamjford@gmail.com>
2013-04-04 16:09:50 -0600

    Quiz8: complete

--- quiz8/gen.py
+++ quiz8/gen.py
@@ -1,10 +1,34 @@
 def mystery(s):
     """
-    WRITE YOUR DOCSTRING HERE.
+    Returns a generator which generates the unique elements
+    in s if they're comparable, in the order in which they
+    first appeared.
+
+    Asymptotic running time: O(n^2)
+
+    >>> list(mystery([]))
+    []
+    >>> list(mystery([1,2,3]))
+    [1, 2, 3]
+    >>> list(mystery([1,4,2,4,3,7,2,4]))
+    [1, 4, 2, 3, 7]
+    >>> g = mystery(['a', 'a', 'b'])
+    >>> next(g)
+    'a'
+    >>> next(g)
+    'b'
+    >>> next(g)
+    Traceback (most recent call last):
+    ...
+    StopIteration
     """
-    
+
     es = set()
     for e in s:
         if e not in es:
             yield e
             es.add(e)
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
